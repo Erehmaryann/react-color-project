4e630beb49b00aaba3d772657f6716272fa0fb0e
Maryann Edward <68599801+maryannedward@users.noreply.github.com>
2021-04-16 13:53:37 +0100

    Saved palettes to localStorage

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import NewPaletteForm from './NewPaletteForm';
 class App extends Component {
 	constructor(props) {
 		super(props);
+		const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
 		this.state = {
-			palettes : seedColors
+			palettes : savedPalettes || seedColors
 		};
 		this.savePalette = this.savePalette.bind(this);
 		this.findPalette = this.findPalette.bind(this);
+		// this.syncLocalStorage = this.syncLocalStorage.bind(this);
 	}
 	findPalette(id) {
 		return this.state.palettes.find(function(palette) {
@@ -22,10 +24,12 @@ class App extends Component {
 	}
 
 	savePalette(newPalette) {
-		// console.log(newPalette);
-		this.setState({ palettes: [ ...this.state.palettes, newPalette ] });
+		this.setState({ palettes: [ ...this.state.palettes, newPalette ] }, this.syncLocalStorage);
 	}
-
+	syncLocalStorage() {
+		// save palettes to local storage
+		window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes));
+	}
 	render() {
 		return (
 			<Switch>
