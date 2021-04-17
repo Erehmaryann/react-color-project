9f7adb8ddfca98174da021297b5938aa825ee2d7
Maryann Edward <68599801+maryannedward@users.noreply.github.com>
2021-04-17 20:26:12 +0100

    Added delete icon to miniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles/MiniPaletteStyles';
 import { withStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function MiniPalette(props) {
 	const { classes, paletteName, emoji, colors, handleClick } = props;
@@ -9,6 +10,9 @@ function MiniPalette(props) {
 	));
 	return (
 		<div className={classes.root} onClick={handleClick}>
+			<div className={classes.delete}>
+				<DeleteIcon className={classes.deleteIcon} style={{ transition: 'all 0.3s ease-in-out' }} />
+			</div>
 			<div className={classes.colors}>{miniColorBoxes}</div>
 			<h5 className={classes.title}>
 				{paletteName} <span className={classes.emoji}>{emoji}</span>
--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -1,24 +1,25 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	root      : {
+	root       : {
 		backgroundColor : '#fff',
 		border          : '1px solid black',
 		borderRadius    : '5px',
 		padding         : '0.5rem',
 		position        : 'relative',
 		overflow        : 'hidden',
-		'&:hover'       : {
-			cursor : 'pointer'
+		cursor          : 'pointer',
+		'&:hover svg'   : {
+			opacity : '1'
 		}
 	},
-	colors    : {
+	colors     : {
 		backgroundColor : '#dae1e4',
 		height          : '100px',
 		width           : '100%',
 		borderRadius    : '5px',
 		overflow        : 'hidden'
 	},
-	title     : {
+	title      : {
 		Display        : 'flex',
 		justifyContent : 'space-between',
 		alignItems     : 'center',
@@ -28,16 +29,29 @@ export default {
 		// fontSize       : '1rem',
 		position       : 'relative'
 	},
-	emoji     : {
+	emoji      : {
 		marginLeft : '0.5rem',
 		fontSize   : '1.2rem'
 	},
-	miniColor : {
+	miniColor  : {
 		height       : '25%',
 		width        : '20%',
 		display      : 'inline-block',
 		margin       : '0 auto',
 		position     : 'relative',
 		marginBottom : '-4.0px'
+	},
+	delete     : {},
+	deleteIcon : {
+		color           : 'white',
+		backgroundColor : '#eb3d30',
+		width           : '20px',
+		height          : '20px',
+		position        : 'absolute',
+		top             : '0px',
+		right           : '0px',
+		padding         : '10px',
+		zIndex          : 10,
+		opacity         : '0'
 	}
 };
