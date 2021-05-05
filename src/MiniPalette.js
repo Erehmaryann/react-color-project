10dcec78bdf3d47065e99343d88bb241db629edd
unknown <edwardmaryann09@gmail.com>
2021-05-04 17:23:19 -0700

    added delete confirmation dialog

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,36 +1,40 @@
-import React, { Component } from 'react';
-import styles from './styles/MiniPaletteStyles';
-import { withStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React, { Component } from "react";
+import styles from "./styles/MiniPaletteStyles";
+import { withStyles } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 class MiniPalette extends Component {
-	constructor(props) {
-		super(props);
-		this.deletePalette = this.deletePalette.bind(this);
-	}
-	deletePalette(e) {
-		e.stopPropagation();
-		this.props.removePalette(this.props.id);
-	}
-	render() {
-		const { classes, paletteName, emoji, colors, handleClick } = this.props;
-		const miniColorBoxes = colors.map((color) => (
-			<div className={classes.miniColor} style={{ backgroundColor: color.color }} key={color.name} />
-		));
-		return (
-			<div className={classes.root} onClick={handleClick}>
-				<DeleteIcon
-					className={classes.deleteIcon}
-					onClick={this.deletePalette}
-					style={{ transition: 'all 0.3s ease-in-out' }}
-				/>
-				<div className={classes.colors}>{miniColorBoxes}</div>
-				<h5 className={classes.title}>
-					{paletteName} <span className={classes.emoji}>{emoji}</span>
-				</h5>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.deletePalette = this.deletePalette.bind(this);
+  }
+  deletePalette(e) {
+    e.stopPropagation();
+    this.props.openDialog(this.props.id);
+  }
+  render() {
+    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    const miniColorBoxes = colors.map((color) => (
+      <div
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+      />
+    ));
+    return (
+      <div className={classes.root} onClick={handleClick}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          onClick={this.deletePalette}
+          style={{ transition: "all 0.3s ease-in-out" }}
+        />
+        <div className={classes.colors}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(MiniPalette);
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,13 +3,45 @@ import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { withStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+import Avatar from "@material-ui/core/Avatar";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import styles from "./styles/PaletteListStyles";
 export class PaletteList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDeleteDialog: false,
+      deletingId: "",
+    };
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  openDialog(id) {
+    this.setState({ openDeleteDialog: true, deletingId: id });
+  }
+  closeDialog() {
+    this.setState({ openDeleteDialog: false, deletingId: "" });
+  }
+  handleDelete() {
+    this.props.removePalette(this.state.deletingId);
+    this.setState(this.closeDialog());
+  }
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
-    const { palettes, classes, removePalette } = this.props;
+    const { palettes, classes } = this.props;
+    const { openDeleteDialog } = this.state;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -19,9 +51,10 @@ export class PaletteList extends Component {
           </nav>
           <TransitionGroup className={classes.palettes}>
             {palettes.map((palette, id) => (
-              <CSSTransition key={palette.id} classNames="fade" timeout={2000}>
+              <CSSTransition key={palette.id} classNames="fade" timeout={500}>
                 <MiniPalette
-                  removePalette={removePalette}
+                  // removePalette={removePalette}
+                  openDialog={this.openDialog}
                   key={id}
                   id={id}
                   {...palette}
@@ -31,6 +64,35 @@ export class PaletteList extends Component {
             ))}
           </TransitionGroup>
         </div>
+        <Dialog
+          open={openDeleteDialog}
+          aria-labelledby="delete-dialog-title"
+          onClose={this.closeDialog}
+        >
+          <DialogTitle id="delete-dialog-title">
+            Delete This Palette?
+          </DialogTitle>
+          <List>
+            <ListItem button onClick={this.handleDelete}>
+              <ListItemAvatar>
+                <Avatar
+                  style={{ backgroundColor: blue[100], color: blue[600] }}
+                >
+                  <CheckIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>Delete</ListItemText>
+            </ListItem>
+            <ListItem button onClick={this.closeDialog}>
+              <ListItemAvatar>
+                <Avatar style={{ backgroundColor: red[100], color: red[600] }}>
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Cancel" />
+            </ListItem>
+          </List>
+        </Dialog>
       </div>
     );
   }
